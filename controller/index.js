7207a378d67db2946868572a09cb678fb789c120
huip <penghui1012@gmail.com>
2015-03-23 00:27:50 +0800

    add wechat menu

--- controller/index.js
+++ controller/index.js
@@ -1,12 +1,17 @@
 var wechat = require('wechat');
 var defaultConfig = require('../config/default');
+var API = wechat.API;
+var api = new API(defaultConfig.wechat.config.appid, defaultConfig.wechat.config.appsecret);
 module.exports = function(app) {
 	app.get('/signup', function(req, res) {
 		res.render('signup');
 	});
 	
-	app.get('/wechat', wechat(defaultConfig.wechat, function(req, res, next) {
+	app.get('/wechat', wechat('wechatToken', function(req, res, next) {
+		api.createMenu(defaultConfig.wechat.menu, function(err, result) {
+			console.log(result);
+		})
 		var message = req.weixin;
 		console.log(message);
-	});
+	}));
 }
